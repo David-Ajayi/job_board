7235278d43a64335e8d53ffc51062658d2746bdd
David Ajayi <davidajayi00@gmail.com>
2021-11-23 15:22:21 +0000

    input field foe user company added and validations to register controller

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -44,6 +44,24 @@
                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                 @enderror
             </div>
+            <div class="mb-6">
+                <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                       for="company"
+                >
+                    Company
+                </label>
+
+                <input class="border border-gray-400 p-2 w-full"
+                       type="text"
+                       name="company"
+                       id="company"
+                       value="{{ old('company') }}"
+                       required
+                >
+                @error('company')
+                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
+            </div>
 
 
 
