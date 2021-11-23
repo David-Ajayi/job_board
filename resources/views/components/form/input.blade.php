f811aacc002526e814927c8ac3785411f64586b6
David Ajayi <davidajayi00@gmail.com>
2021-11-23 13:13:35 +0000

    sets value to be a named attribute

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,4 +1,4 @@
-@props(['name', 'type' => 'text'])
+@props(['name'])
 {{--type maps to a type of text. passed through as a prop. type will be text as default unless overidden as we do--}}
 {{--with the file type for out thumbnail field--}}
 
@@ -7,11 +7,10 @@
     <x-form.label name="{{ $name }}"/>
 
     <input class="border border-gray-400 p-2 w-full"
-           type="{{ $type }}"
+{{--           type="{{ $type }}"--}}
            name="{{ $name }}"
            id="{{ $name }}"
-           value="{{ old($name) }}"
-           required
+           {{$attributes (['value' => old($name)]) }}
     >
 
     <x-form.error name="{{ $name }}"/>
