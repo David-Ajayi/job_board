318a5736605ff65eb8bf0142a26cf81baefcb633
David Ajayi <davidajayi00@gmail.com>
2021-11-24 08:23:12 +0000

    disabled MustBeAdministrator middleware and defined admin gate in appServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Providers;
-
+use App\Models\User;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Model;
 
@@ -26,5 +28,13 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Model::unguard();
+
+        Gate::define('admin', function (User $user) {
+            return $user->username === 'Jwick' || 'admin';
+        });
+
+        Blade::if('admin', function () {
+            return request()->user()?->can('admin');
+        });
     }
 }
